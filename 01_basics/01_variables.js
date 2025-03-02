6a960d0b7a0cafecc2b8fad08ee5b52a5ad87b0e
Jahid Sheikh <pixmjahid9428@gmail.com>
2025-03-02 13:31:22 +0530

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = "123456";
+let accountEmail = "jahid@google.com";
+var accountPassword = "1234";
+accountCity = "jaipur";
+let accountState;
+
+// accountId = 4 not allowed
+
+accountEmail = "parveen@google.com";
+accountPassword = "343433434";
+accountCity = "sikar";
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,18 @@
-console.log("Jahid")
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+const filepath = path.join(__dirname, "text.js");
+fs.readFile(filepath, "utf8", (err, data) => {
+  if (err) {
+    console.log("error");
+  }
+  console.log(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running ${PORT}`);
+});
